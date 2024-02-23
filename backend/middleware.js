580f0d7edc2250cf0438946f59b040f6ea165317
Zahid <zahidmohammad495@gmai.com>
2024-02-23 20:07:55 +0530

    remove old frontend folder

--- backend/middleware.js
+++ backend/middleware.js
@@ -10,7 +10,6 @@ const authMiddleware = (req, res, next) => {
   }
   const token = authHeader.split(" ")[1];
 
-  
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.userId;
